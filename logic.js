25f655673b120243ed2b9e6d01240d25ecf43fc7
auztin410 <auztin410@gmail.com>
2018-06-18 09:49:35 -0400

    Third Commit

--- logic.js
+++ logic.js
@@ -1,16 +1,22 @@
 
-var counter = 0;
+var counter = [];
 var gameReady = true;
 var randomNumber = 0;
 var redRandom = 0;
 var blueRandom = 0;
 var yellowRandom = 0;
 var purpleRandom = 0;
+var testCounter = 0;
+
+function sum(numbers) {
+    return numbers.reduce(function(a,b) {
+      return a + b
+    });
+  }
 
 function createNumber() {
- randomNumber=[Math.floor(Math.random() * 200) +50];
- console.log("this is working!")
-}
+ randomNumber=[Math.floor(Math.random() * 100) +50];
+ }
 
 function redNumber() {
 redRandom=[Math.floor(Math.random() * 10) +1];
@@ -37,15 +43,132 @@ yellowNumber();
 purpleNumber();
 };
 
-    
+function empty() {
+        counter.length = 0;
+}
+
+function sum(input){
+             
+    if (toString.call(input) !== "[object Array]")
+       return false;
+         
+               var total =  0;
+               for(var i=0;i<input.length;i++)
+                 {                  
+                   if(isNaN(input[i])){
+                   continue;
+                    }
+                     total += Number(input[i]);
+                  }
+                return total;
+               }
 
 $("#startButton").on("click", function () {
     startGame();
-    console.log("Your click worked!");
-    console.log(gameReady);
-    console.log(randomNumber);
-    console.log(redRandom);
-    console.log(blueRandom);
-    console.log(yellowRandom);
-    console.log(purpleRandom);
+    document.getElementById("numberToGuess").innerHTML= randomNumber;
+    empty(counter);
+    testCounter = 0;
+    document.getElementById("currentCounter").innerHTML= sum(counter);
+    // console.log(gameReady);
+    // console.log(randomNumber);
+    // console.log(redRandom);
+    // console.log(blueRandom);
+    // console.log(yellowRandom);
+    // console.log(purpleRandom);
+});
+
+$("#red").on("click", function () {
+    if (gameReady == true) {
+    }
+
+    if (gameReady == false) {
+        counter.push(redRandom);
+        document.getElementById("currentCounter").innerHTML= sum(counter);
+        redRandom = parseInt(redRandom);
+        testCounter += redRandom;
+        // console.log(testCounter);
+        
+    }
+    
+    if (gameReady == false && testCounter == randomNumber) {
+        alert("You Win!");
+    }
+
+    else if (gameReady == false && testCounter >= randomNumber) {
+        alert("You Lose");        
+    }
+
+    
+});
+
+$("#blue").on("click", function () {
+    if (gameReady == true) {
+    }
+
+    if (gameReady == false) {
+        counter.push(blueRandom);
+        document.getElementById("currentCounter").innerHTML= sum(counter);
+        blueRandom = parseInt(blueRandom);
+        testCounter += blueRandom;
+        // console.log(sum(counter));
+        
+    }
+    
+    if ( gameReady == false && testCounter == randomNumber) {
+        alert("You Win!");        
+    }
+
+    else if (gameReady == false && testCounter >= randomNumber) {
+        alert("You Lose");        
+    }
+
+    
+});
+
+$("#yellow").on("click", function () {
+    if (gameReady == true) {
+    }
+
+    if (gameReady == false) {
+        counter.push(yellowRandom);
+        document.getElementById("currentCounter").innerHTML= sum(counter);
+        yellowRandom = parseInt(yellowRandom);
+        testCounter += yellowRandom;
+        // console.log(sum(counter));
+        
+    }
+    
+    if (gameReady == false && testCounter == randomNumber) {
+        alert("You Win!");        
+    }
+
+    else if (gameReady == false && testCounter >= randomNumber) {
+        alert("You Lose");        
+    }
+
+    
+});
+
+$("#purple").on("click", function () {
+    if (gameReady == true) {
+    }
+
+    if (gameReady == false) {
+        counter.push(purpleRandom);
+        document.getElementById("currentCounter").innerHTML= sum(counter);
+        purpleRandom = parseInt(purpleRandom);
+        testCounter += purpleRandom;
+        // console.log(sum(counter));
+        
+    }
+    
+    if (gameReady == false && testCounter == randomNumber) {
+        alert("You Win!");        
+    }
+
+    else if (gameReady == false && testCounter >= randomNumber) {
+        alert("You Lose");        
+    }
+
+    
 });
